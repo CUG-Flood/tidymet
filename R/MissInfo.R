dd3d413c2721d604bcee75733316426d3390fa67
kongdd <kongdd@mail2.sysu.edu.cn>
2016-11-13 20:10:06 +0800

    MissInfo package init

---
+++ R/MissInfo.R
@@ -0,0 +1,12 @@
+#' @title MissInfo
+#' @name MissInfo
+#' @aliases MissInfo-package
+#' @docType package
+#' @keywords missing information, interpolate
+#' @description detect missing information of meteorological data and interpolate missing values using common interpolation methods
+#' @import magrittr
+NULL
+
+# .onUnload <- function (libpath) {
+#   library.dynam.unload("MissInfo", libpath)
+# }
