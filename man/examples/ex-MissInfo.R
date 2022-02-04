a82fd00463ceb36eb0e96a4f13327d1685f32de2
Dongdong Kong <kongdd@users.noreply.github.com>
2022-02-04 21:18:11 +0800

    fix missInfo typo error

--- man/examples/ex-MissInfo.R
+++ man/examples/ex-MissInfo.R
@@ -2,6 +2,6 @@ data('prcp')
 time_day <- prcp$date
 site <- colnames(prcp)[-1]
 mat  <- as.matrix(prcp[, -1])
-Info1 <- missInfo:::MissInfo.matrix(mat, clipdata = TRUE, time_day, site, verbose = TRUE)
-Info2 <- MissInfo(prcp, clipdata = TRUE, time_day, site, verbose = TRUE)
+Info1 <- missInfo(mat, clipdata = TRUE, time_day, site, verbose = TRUE)
+Info2 <- missInfo(prcp, clipdata = TRUE, time_day, site, verbose = TRUE)
 all.equal(Info2, Info1)
